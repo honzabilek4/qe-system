d73de43b669238d746bf2361b40d0b298231e1ed
Jan Bílek <honza@elisiondesign.cz>
2018-09-18 23:40:30 +0200

    Update docker project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('hello world')
 });
 
-app.listen(3000);
+app.listen(process.env.PORT);
