66de10869d6bd84bdc8fd91eebd2758252b3b69f
Jan Bílek <honza@elisiondesign.cz>
2018-10-12 01:11:34 +0200

    Add dynamic table name

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const { Client } = require('pg');
 
 const app = express();
 
-app.get('/', (request, response) => {
+
+app.get('/:table', (request, response) => {
   const client = new Client(process.env.CONNECTION_STRING);
   client.connect().catch((e) => {
     console.log(e);
   });
-  client.query('SELECT NOW()', (err, res) => {
+  const query = `SELECT * FROM ${request.params.table}`;
+  client.query(query, (err, res) => {
+    console.log(err, res);
     response.send(res);
     client.end();
   });
