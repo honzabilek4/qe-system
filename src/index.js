58f7505c49cd44dd00885cdff8e18e3b5e5868b1
Jan Bílek <honza@elisiondesign.cz>
2018-10-12 00:39:45 +0200

    Add test select command

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const { Client } = require('pg');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hey man!');
+app.get('/', (request, response) => {
+  const client = new Client(process.env.CONNECTION_STRING);
+  client.connect().catch((e) => {
+    console.log(e);
+  });
+  client.query('SELECT NOW()', (err, res) => {
+    response.send(res);
+    client.end();
+  });
 });
+
 app.listen(process.env.PORT);
