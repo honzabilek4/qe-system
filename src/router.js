e49018969a412d0b34f7dd96371e8292099bbda4
Jan Bílek <honza@elisiondesign.cz>
2018-10-12 09:53:45 +0200

    Select using Id

--- src/router.js
+++ src/router.js
@@ -16,13 +16,25 @@ router.get('/:table', (request, response) => {
   });
 });
 
+router.get('/:table/:id', (request, response) => {
+  const client = new Client(process.env.CONNECTION_STRING);
+  client.connect().catch((e) => {
+    console.log(e);
+  });
+  const query = squel.select().from(request.params.table).where('id=?', request.params.id).toString();
+  client.query(query, (err, res) => {
+    response.send(res);
+    client.end();
+  });
+});
+
 router.post('/:table', (request, response) => {
   const client = new Client(process.env.CONNECTION_STRING);
   client.connect().catch((e) => {
     console.log(e);
   });
   const query = squel.insert().into(request.params.table).setFields(request.body).toString();
-  client.query(query, (err, res) => {
+  client.query(query, () => {
     // console.log(err, res);
     response.send(request.body);
     client.end();
