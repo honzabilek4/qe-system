08bf1ae375972e9e8f25049b3858c4611af1c601
Jan Bílek <honza@elisiondesign.cz>
2018-10-21 23:57:54 +0200

    Change router to knexjs

--- src/router.js
+++ src/router.js
@@ -14,34 +14,33 @@ router.get('/:table', (request, response) => {
   });
   const query = SqlBuilder.select().from(request.params.table).toString();
   client.query(query, (err, res) => {
-    response.send(res);
+    response.send(res.rows);
     client.end();
   });
 });
 
-// router.get('/:table/:id', (request, response) => {
-//   const client = new Client(process.env.CONNECTION_STRING);
-//   client.connect().catch((e) => {
-//     console.log(e);
-//   });
-//   const query = squel.select().from(request.params.table).where('id=?', request.params.id).toString();
-//   client.query(query, (err, res) => {
-//     response.send(res);
-//     client.end();
-//   });
-// });
+router.get('/:table/:id', (request, response) => {
+  const client = new Client(process.env.CONNECTION_STRING);
+  client.connect().catch((e) => {
+    console.log(e);
+  });
+  const query = SqlBuilder.select().from(request.params.table).where({ id: request.params.id }).toString();
+  client.query(query, (err, res) => {
+    response.send(res.rows);
+    client.end();
+  });
+});
 
-// router.post('/:table', (request, response) => {
-//   const client = new Client(process.env.CONNECTION_STRING);
-//   client.connect().catch((e) => {
-//     console.log(e);
-//   });
-//   const query = squel.insert().into(request.params.table).setFields(request.body).toString();
-//   client.query(query, () => {
-//     // console.log(err, res);
-//     response.send(request.body);
-//     client.end();
-//   });
-// });
+router.post('/:table', (request, response) => {
+  const client = new Client(process.env.CONNECTION_STRING);
+  client.connect().catch((e) => {
+    console.log(e);
+  });
+  const query = SqlBuilder(request.params.table).insert(request.body).toString();
+  client.query(query, () => {
+    response.send(request.body);
+    client.end();
+  });
+});
 
 module.exports = router;
