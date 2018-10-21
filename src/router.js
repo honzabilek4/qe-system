20558e6c7af72028d48336bb32aa0bc3433ed4a7
Jan Bílek <honza@elisiondesign.cz>
2018-10-22 00:34:33 +0200

    Rewrite knex queries

--- src/router.js
+++ src/router.js
@@ -1,5 +1,4 @@
 const { Router } = require('express');
-const { Client } = require('pg');
 const SqlBuilder = require('knex')({
   client: 'pg',
   connection: process.env.CONNECTION_STRING,
@@ -8,39 +7,33 @@ const SqlBuilder = require('knex')({
 const router = Router();
 
 router.get('/:table', (request, response) => {
-  const client = new Client(process.env.CONNECTION_STRING);
-  client.connect().catch((e) => {
-    console.log(e);
-  });
-  const query = SqlBuilder.select().from(request.params.table).toString();
-  client.query(query, (err, res) => {
-    response.send(res.rows);
-    client.end();
-  });
+  SqlBuilder(request.params.table).select()
+    .then((data) => {
+      response.send(data).json();
+    })
+    .catch((error) => {
+      response.send(error).json();
+    });
 });
 
 router.get('/:table/:id', (request, response) => {
-  const client = new Client(process.env.CONNECTION_STRING);
-  client.connect().catch((e) => {
-    console.log(e);
-  });
-  const query = SqlBuilder.select().from(request.params.table).where({ id: request.params.id }).toString();
-  client.query(query, (err, res) => {
-    response.send(res.rows);
-    client.end();
-  });
+  SqlBuilder(request.params.table).select().where({ id: request.params.id })
+    .then((data) => {
+      response.send(data).json();
+    })
+    .catch((error) => {
+      response.send(error).json();
+    });
 });
 
 router.post('/:table', (request, response) => {
-  const client = new Client(process.env.CONNECTION_STRING);
-  client.connect().catch((e) => {
-    console.log(e);
-  });
-  const query = SqlBuilder(request.params.table).insert(request.body).toString();
-  client.query(query, () => {
-    response.send(request.body);
-    client.end();
-  });
+  SqlBuilder(request.params.table).insert(request.body)
+    .then(() => {
+      response.send(request.body).json();
+    })
+    .catch((error) => {
+      response.send(error).json();
+    });
 });
 
 module.exports = router;
