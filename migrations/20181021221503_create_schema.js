c6f4170b734a26ee5342da1d9ac597ca3800f529
Jan Bílek <honza@elision.design>
2018-11-18 11:53:22 +0100

    modifi create schema

--- migrations/20181021221503_create_schema.js
+++ migrations/20181021221503_create_schema.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
 
-<<<<<<< HEAD
 exports.up = function up(knex, Promise) {
-=======
-exports.up = function (knex, Promise) {
->>>>>>> knexjs
   const sqlQuery = fs.readFileSync('./migrations/sql/qesystem_21_10_2018.sql', 'utf8');
   return knex.schema.raw(sqlQuery.toString());
 };
